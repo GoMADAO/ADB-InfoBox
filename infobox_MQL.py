3aa332ed9fa3513c181c93ed1767842a39247dbe
gmf <mg3534@columbia.edu>
2015-03-21 04:49:53 -0400

    change input format, pretty print query name and type, unfinished work: pretty print infobox

--- infobox_MQL.py
+++ infobox_MQL.py
@@ -1,7 +1,10 @@
 import json
 import urllib
-
 import sys
+
+from pprint import pprint
+# For parse the input strings
+from argparse import ArgumentParser
 
 # For regular expression
 import re
@@ -9,9 +12,9 @@ import re
 from operator import itemgetter
 
 # function to do Search API call
-def searchQuery(query):
+def searchQuery(query, api_key):
 
-	api_key = open("api_key.txt").read()
+	#api_key = open("api_key.txt").read()
 	# print api_key
 	# query = 'blue bottle'
 	service_url = 'https://www.googleapis.com/freebase/v1/search'
@@ -24,9 +27,9 @@ def searchQuery(query):
 	return response
 
 # function to do Topic API call
-def topicQuery(topic_id):
+def topicQuery(topic_id, api_key):
 
-	api_key = open("api_key.txt").read()
+	#api_key = open("api_key.txt").read()
 	service_url = 'https://www.googleapis.com/freebase/v1/topic'
 	# topic_id = '/m/016z2j' # id of actor
 	params = {
@@ -63,7 +66,6 @@ def infoExtractor(pattern, infoBox, topicResult):
 	# infoBox = {}
 
 	# Build a dictionary of the entities we are interested in
-	# pattern is actually a configuration file
 	with open(pattern,"r") as text:
 		for line in text:
 			prop = line.strip().split('-')
@@ -77,20 +79,19 @@ def infoExtractor(pattern, infoBox, topicResult):
 					keyValue = pair.split('+') 
 					propertyDict[prop[0]][keyValue[0]] = keyValue[1]
 
-	print propertyDict
+	#print propertyDict
 
 	# Extract properties from the Topic API response
 	for prop in propertyDict:
 		if prop in topicResult['property']:
 			temp = topicResult['property'][prop]
-			print prop
-			print "Pattern matching"
+			#print prop
+			#print "Pattern matching"
 			if type(propertyDict[prop]) is dict:
 				# array of "values"
-				print "multiple entries"
+				#print "multiple entries"
 				# multiple property needs to be extracted
 
-				# By design, this would find the right name to be key
 				for subprop2 in propertyDict[prop]:
 					matchObj = re.match(r'.*'+re.escape(propertyDict[prop][subprop2])+r'.*', subprop2, re.I)
 					if matchObj:
@@ -101,14 +102,13 @@ def infoExtractor(pattern, infoBox, topicResult):
 				
 				tempDict = {}
 
-				# Extract the data we are interested in and build a dictionary
 				for entry in temp['values']:
 					temp2 = entry['property']
 					
 					for subprop in propertyDict[prop]:
 						if subprop in temp2:
-							print subprop
-							print temp2[subprop]['values']
+							#print subprop
+							#print temp2[subprop]['values']
 							if temp2[subprop]['values'] != []:
 								tempDict[propertyDict[prop][subprop]] = temp2[subprop]['values'][0]['text']
 							else:
@@ -128,15 +128,21 @@ def infoExtractor(pattern, infoBox, topicResult):
 						else:
 							infoBox[propertyDict[prop]].append(result['text'])
 
-					print infoBox[propertyDict[prop]]
+					#print infoBox[propertyDict[prop]]
 				
 			
 	
-	# print infoBox
+	#print infoBox
+
+def printInfobox():	
+	with open("infoBox.txt","r") as data_file:
+		data = json.load(data_file)
+	pprint(data)
+
 
 # Do MQL query
-def mqlQuery(query):
-	api_key = open("api_key.txt").read()
+def mqlQuery(query, api_key):
+	#api_key = open("api_key.txt").read()
 	service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
 
 	# We are only looking for two types -- Book and Organizations
@@ -157,21 +163,21 @@ def mqlQuery(query):
 	url = service_url + '?' + urllib.urlencode(params)
 	response = json.loads(urllib.urlopen(url).read())
 
-	# query2 = [{
-	#   	"/organization/organization_founder/organizations_founded": [{
-	#   	  "a:name": None,
-	#   	  "name~=": query
-	#   	}],
-	#   	"id": None,
-	#   	"name": None,
-	#   	"type": "/organization/organization_founder"
-	# }]
-	# params2 = {
-	#         'query': json.dumps(query2),
-	#         'key': api_key
-	# }
-	# url2 = service_url + '?' + urllib.urlencode(params2)
-	# response2 = json.loads(urllib.urlopen(url2).read())
+	query2 = [{
+       	"/organization/organization_founder/organizations_founded": [{
+        "a:name": None,
+        "name~=": query
+	   	}],
+	   	"id": None,
+	   	"name": None,
+	   	"type": "/organization/organization_founder"
+	 }]
+	params2 = {
+	         'query': json.dumps(query2),
+	         'key': api_key
+	}
+	url2 = service_url + '?' + urllib.urlencode(params2)
+	response2 = json.loads(urllib.urlopen(url2).read())
 	
 	# return response
 	return response
@@ -182,7 +188,6 @@ def jsonWrite(data, fileName):
 		json.dump(data, outfile, sort_keys = True, indent = 4)
 
 def printResponse(data, queryType):
-	# Different entities to look at for book and organization
 	if queryType == 'book':
 		key = '/book/author/works_written'
 	elif queryType == 'organization':
@@ -191,10 +196,8 @@ def printResponse(data, queryType):
 	string = ''
 	index = 0
 
-	# Sort the returned result according the 'name' value
 	newlist = sorted(data['result'], key=itemgetter('name')) 
 
-	# Print on the terminal
 	if newlist != []:
 		for entry in newlist:
 			index += 1
@@ -205,67 +208,132 @@ def printResponse(data, queryType):
 
 			print string
 
-	
+def printRunFormat():
+		print "Run the script by following format:"
+		print "python infobox_MQL.py -k <Freebase API key> -q <query> -t <infobox|question>"
+		print "python infobox_MQL.py -k <Freebase API key> -f <file of queries> -t <infobox|question>"	
+
+def printEntityDict(entityDic, query):
+	displayDic={'AUTHOR':False, 'BUSINESS_PERSON':False, 'ACTOR':False,
+			'LEAGUE':False, 'SPORTS TEAM':False }
+	for item in entityDic:
+		if entityDic[item]['found']==True and item == '/sports/sports_league':
+			displayDic['LEAGUE']=True
+		if entityDic[item]['found']==True and item ==\
+			'/sports/professional_sports_team':
+			displayDic['SPORTS TEAM']=True
+		if entityDic[item]['found']==True and item == '/book/author':
+			displayDic['AUTHOR']=True
+		if entityDic[item]['found']==True and item == '/tv/tv_actor':
+			displayDic['ACTOR']=True
+		if entityDic[item]['found']==True and item == '/film/actor':
+			displayDic['ACTOR']=True
+		if entityDic[item]['found']==True and item == '/business/board_member':
+			displayDic['BUSINESS_PERSON']=True
+		if entityDic[item]['found']==True and item ==\
+		'/organization/organization_founder':
+			displayDic['BUSINESS_PERSON']=True
+
+	print ' ------------------------------------------------------'+\
+			'---------------------------------------------'
+
+	temp_str=''
+	for item in displayDic:
+		if displayDic[item]==True:
+			temp_str+= item+','
+
+	temp_str= query+'('+temp_str[0:len(temp_str)-1]+')'
+	print '|'+'{:^99}'.format(temp_str)+'|'
+	print ' ------------------------------------------------------'+\
+			'---------------------------------------------'
+
 
 def main():
 	# Taking query from command line
-	if sys.argv[1] == '-q':
-		query = sys.argv[2]
 
-	if sys.argv[3] == '-t':
-		queryType = sys.argv[4]
+	parser = ArgumentParser()
 
-		if queryType == 'infobox':
-			# query = "Robert Downey Jr."
-			print query
-			searchResult = searchQuery(query)
-			jsonWrite(searchResult, 'search_response.txt')
+	parser.add_argument('-k', '--key', nargs=1, required=True,\
+			dest="key", help='input API Key')
+	parser.add_argument('-q', '--query', nargs='*', dest='query', help='input query')
+	parser.add_argument('-t', '--type', nargs=1, dest='qtype', help='input query type')
+	parser.add_argument('-f', '--file',  nargs=1, dest='filename', help='input filename')
 
-			# To keep track of all the existing entities that we are interested in
-			entityDict = {}
-			# To store the information extracted from Topic API response
-			infoBox = {}
+	args=parser.parse_args()
+	api_key = args.key
+	querylist = args.query
+	queryType = args.qtype
+	filename = args.filename
 
-			# iteration = 0
+	if filename ==None and querylist==None:
+		printRunFormat()
+		exit()
 
-			# Keep calling Topic API until it finds a response that contains at least
-			# one of six entities we are interested in
-		 	for result in searchResult['result']:
-		 		topicResult = topicQuery(result['mid'])
-				entityDict, match = matchEntity(topicResult['property']['/type/object/type']['values'])
+	api_key=''.join(api_key)
+	query= ' '.join(querylist)
+	queryType= ''.join(queryType)
+	#filename=filename 
+	
 
-				if match == True:
-					break
-				# iteration += 1
+	if queryType == 'infobox':
+		# query = "Robert Downey Jr."
+		#print query
+		searchResult = searchQuery(query, api_key)
+		jsonWrite(searchResult, 'search_response.txt')
 
-			jsonWrite(topicResult, 'topic_response.txt')
+		# To keep track of all the existing entities that we are interested in
+		entityDict = {}
+		# To store the information extracted from Topic API response
+		infoBox = {}
 
-			infoExtractor('person_property.txt', infoBox, topicResult)
+		# iteration = 0
 
-			infoExtractor('author_property.txt', infoBox, topicResult)
+	 	for result in searchResult['result']:
+	 		topicResult = topicQuery(result['mid'], api_key)
+			entityDict, match = matchEntity(topicResult['property']['/type/object/type']['values'])
 
-			infoExtractor('actor_property.txt', infoBox, topicResult)
+			if match == True:
+				break
+			# iteration += 1
 
-			infoExtractor('businessperson_property.txt', infoBox, topicResult)
+		#print entityDict
 
-			infoExtractor('sportsteam_property.txt', infoBox, topicResult)
+		
+		printEntityDict(entityDict,query)
+		jsonWrite(topicResult, 'topic_response.txt')
 
-			infoExtractor('league_property.txt', infoBox, topicResult)
+		infoExtractor('person_property.txt', infoBox, topicResult)
 
-			jsonWrite(infoBox, 'infoBox.txt')
+		infoExtractor('author_property.txt', infoBox, topicResult)
 
-		elif queryType == 'question':
+		infoExtractor('actor_property.txt', infoBox, topicResult)
 
-			matchObj = re.match(r'Who created (.*?)\?', query, re.I)
-			query = matchObj.group(1)
+		infoExtractor('businessperson_property.txt', infoBox, topicResult)
 
-			print query
+		infoExtractor('sportsteam_property.txt', infoBox, topicResult)
 
-			response = mqlQuery(query)
+		infoExtractor('league_property.txt', infoBox, topicResult)
 
-			printResponse(response, 'book')
-			jsonWrite(response, 'book_mql.txt')
-			# jsonWrite(response2, 'founder_mql.txt')
+		jsonWrite(infoBox, 'infoBox.txt')
+
+		printInfobox()
+
+	elif queryType == 'question':
+		# print query
+
+		matchObj = re.match(r'Who created (.*?)\?', query, re.I)
+		query = matchObj.group(1)
+
+		# Get rid of the question mark
+		# if query[-1] == '?':
+		# 	query = query[:-1]
+		#print query
+
+		response = mqlQuery(query, api_key)
+
+		printResponse(response, 'book')
+		jsonWrite(response, 'book_mql.txt')
+		# jsonWrite(response2, 'founder_mql.txt')
 	
 
 if __name__ == '__main__':
